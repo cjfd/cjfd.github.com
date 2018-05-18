b78d562b3b60c4c55b421c2317f59a432b61cafd
Aaronwn <aaronwn@foxmail.com>
2018-05-18 14:36:40 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,21 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
+const home = resolve => require(['../components/common/Home.vue'], resolve)
+const dashboard = resolve => require(['../components/page/Dashboard.vue'], resolve)
+const table = resolve => require(['../components/page/BaseTable.vue'], resolve)
+const tab = resolve => require(['../components/page/Tabs.vue'], resolve)
+const form = resolve => require(['../components/page/BaseForm.vue'], resolve)
+const editor = resolve => require(['../components/page/VueEditor.vue'], resolve)
+const markdown = resolve => require(['../components/page/Markdown.vue'], resolve)
+const upload = resolve => require(['../components/page/Upload.vue'], resolve)
+const drag = resolve => require(['../components/page/DragList.vue'], resolve)
+const permission = resolve => require(['../components/page/Permission.vue'], resolve)
+const login = resolve => require(['../components/page/Login.vue'], resolve)
+const error404 = resolve => require(['../components/page/404.vue'], resolve)
+const error403 = resolve => require(['../components/page/403.vue'], resolve)
+
+
 export default new Router({
     routes: [
         {
@@ -11,72 +26,72 @@ export default new Router({
         },
         {
             path: '/',
-            component: resolve => require(['../components/common/Home.vue'], resolve),
+            component: home,
             meta: { title: '自述文件' },
-            children:[
+            children: [
                 {
                     path: '/dashboard',
-                    component: resolve => require(['../components/page/Dashboard.vue'], resolve),
+                    component: dashboard,
                     meta: { title: '系统首页' }
                 },
                 {
                     path: '/table',
-                    component: resolve => require(['../components/page/BaseTable.vue'], resolve),
+                    component: table,
                     meta: { title: '基础表格' }
                 },
                 {
                     path: '/tabs',
-                    component: resolve => require(['../components/page/Tabs.vue'], resolve),
+                    component: tab,
                     meta: { title: 'tab选项卡' }
                 },
                 {
                     path: '/form',
-                    component: resolve => require(['../components/page/BaseForm.vue'], resolve),
+                    component: form,
                     meta: { title: '基本表单' }
                 },
                 {
                     // 富文本编辑器组件
                     path: '/editor',
-                    component: resolve => require(['../components/page/VueEditor.vue'], resolve),
+                    component: editor,
                     meta: { title: '富文本编辑器' }
                 },
                 {
                     // markdown组件
                     path: '/markdown',
-                    component: resolve => require(['../components/page/Markdown.vue'], resolve),
-                    meta: { title: 'markdown编辑器' }    
+                    component: markdown,
+                    meta: { title: 'markdown编辑器' }
                 },
                 {
                     // 图片上传组件
                     path: '/upload',
-                    component: resolve => require(['../components/page/Upload.vue'], resolve),
-                    meta: { title: '文件上传' }   
+                    component: upload,
+                    meta: { title: '文件上传' }
                 },
                 {
                     // 拖拽列表组件
                     path: '/drag',
-                    component: resolve => require(['../components/page/DragList.vue'], resolve),
+                    component: drag,
                     meta: { title: '拖拽列表' }
                 },
                 {
                     // 权限页面
                     path: '/permission',
-                    component: resolve => require(['../components/page/Permission.vue'], resolve),
+                    component: permission,
                     meta: { title: '权限测试', permission: true }
                 }
             ]
         },
         {
             path: '/login',
-            component: resolve => require(['../components/page/Login.vue'], resolve)
+            component: login
         },
         {
             path: '/404',
-            component: resolve => require(['../components/page/404.vue'], resolve)
+            component: error404
         },
         {
             path: '/403',
-            component: resolve => require(['../components/page/403.vue'], resolve)
+            component: error403
         },
         {
             path: '*',
